7c7e3ce7ed865c76b28ea209d4a67acbcafd7c46
Sei666 <sei666@rambler.ru>
2020-06-18 11:56:46 +0300

    achive_norm_work

--- js/eventArchive/calendar.js
+++ js/eventArchive/calendar.js
@@ -2,11 +2,17 @@ let monthName = ["Январь", "Февраль" , "Март" , "Апрель" 
 let calendarHeader;
 let childCalendarHeader;
 let monthButtons;
+let monthBlocks = []
+
 document.addEventListener("DOMContentLoaded", function() {
+    var yearInPath = window.location.pathname.split("/");
+
+
     calendarHeader = document.getElementsByClassName("CalendarArchiveHeader")[0];
     childCalendarHeader = calendarHeader.childNodes;
-    childCalendarHeader[1].innerText = "";
-
+    childCalendarHeader[0].innerText = yearInPath[3];
+    //childCalendarHeader[1].innerText = "";
+    console.log(window.location.host);
     monthButtons = document.getElementsByClassName("Month");
     for (let i = 0; i<12; i++){
         monthButtons[i].onclick = function(event) {
@@ -20,57 +26,101 @@ document.addEventListener("DOMContentLoaded", function() {
             monthSelect(monthIndex);
 
         };
+
+        monthBlocks.push(document.getElementById((i+1) + "_month"));
+        if (monthBlocks[i] != null){
+            var a = monthBlocks[i].childNodes[1].childNodes[1].innerText = monthName[i];
+            // a = a[1];
+            // a = a.childNodes;
+            // a = a[1];
+            // a.innerText = monthName[i];
+        }
+        else{
+            document.getElementById("month_" + i).style.backgroundColor = "red";
+        }
+        
+
         //monthButtons[i].addEventListener('click', function(i){ alert(i);}, false);
     }
-
+    console.log(monthBlocks);
 });
 
 
 function monthSelect(monthIndex) {
     monthButtonsStateNormal();
-    childCalendarHeader[1].innerText = monthName[monthIndex];
+    //childCalendarHeader[1].innerText = monthName[monthIndex];
     monthButtons[monthIndex].classList.add("MonthActive");
-    var year = childCalendarHeader[0].innerText;
-    getConcert(year, monthIndex);
+    //var year = childCalendarHeader[0].innerText;
+    //getConcert(monthIndex);
+    
+    if (monthBlocks[monthIndex] != null){
+        monthBlocks[monthIndex].style.display = "block";
+        document.getElementById("messageNotFound").style.display = "none";
+    }
+    else{
+        document.getElementById("messageNotFound").style.display = "block";
+    }
 }
 
 function monthButtonsStateNormal(){
-    //console.log(monthButtons);
+    //console.log("asdasda");
     for (let i = 0; i<12; i++){
         monthButtons[i].classList.remove("MonthActive");
+        if (monthBlocks[i] != null){
+            monthBlocks[i].style.display = "none";
+        }
+        
+    }
+}
+
+function showAllYear(){
+    document.getElementById("messageNotFound").style.display = "none";
+    for (let i = 0; i<12; i++){
+        if (monthBlocks[i] != null){
+            monthBlocks[i].style.display = "block";
+        }
+        
     }
 }
 
 function arrowYearChange(direction) {
     //alert(direction);
-    monthButtonsStateNormal();
+    // monthButtonsStateNormal();
     var year = childCalendarHeader[0].innerText;
-    if (parseInt(year) != NaN){
-        year = (parseInt(year) + direction).toString();
-
+    var intYear = parseInt(year);
+    if (intYear != NaN){
+        if ((intYear + direction) >= startYear && (intYear + direction) <= endYear){
+            year = (intYear + direction).toString();
+            // childCalendarHeader[0].innerText = year;
+            window.location.assign(window.location.protocol + "//" + window.location.host + "/templates/archive/" + year + "/year.php");
+        }
     }
-    childCalendarHeader[0].innerText = year;
-    childCalendarHeader[1].innerText = "";
-    getConcert(year,-1);
+    // childCalendarHeader[0].innerText = year;
+    // childCalendarHeader[1].innerText = "";
+    // getConcert(year,-1);
     //alert("aaasssss");
 }
 
-function getConcert(year, month) {
-    $.ajax({
-        url: window.location.protocol + "//" + window.location.host + "/getArcEvents?year=" + year + "&month=" + month,
-        method: 'GET',
-        type: 'GET',
+// function getConcert(year, month) {
+//     $.ajax({
+//         url: window.location.protocol + "//" + window.location.host + "/getArcEvents?year=" + year + "&month=" + month,
+//         method: 'GET',
+//         type: 'GET',
 
-        success: function (data) {
-            console.log(data);
-            // var deletedPost = document.getElementById("idPost_" + id);
-            // deletedPost.children[1].style.display = "none";
-            // deletedPost.children[0].style.display = "block";
+//         success: function (data) {
+//             console.log(data);
+//             // var deletedPost = document.getElementById("idPost_" + id);
+//             // deletedPost.children[1].style.display = "none";
+//             // deletedPost.children[0].style.display = "block";
 
 
-        },
-        error: function (e) {
-            alert(e.responseText);
-        }
-    });
-}
+//         },
+//         error: function (e) {
+//             alert(e.responseText);
+//         }
+//     });
+// }
+
+// function getConcert(monthIndex){
+//     console.log(monthIndex);
+// }
